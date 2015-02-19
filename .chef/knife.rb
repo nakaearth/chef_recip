e789de2ee9aa6b8503eaffdb1ab2efa7fd00b74d
nakamura shinichirou <Nakamura.Shinichiro@moneyforward.co.jp>
2015-02-19 21:35:43 +0900

    途中コミット

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,1 +1,6 @@
 local_mode true
+#current_dir = File.absolute_path( File.dirname(__FILE__) )
+#cookbook_path ["#{current_dir}/cookbooks"]
+#node_path     "#{current_dir}/nodes"
+#role_path     "#{current_dir}/roles"
+ssl_verify_mode  :verify_peer
